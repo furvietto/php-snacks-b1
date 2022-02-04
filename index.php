bbd37cb0e4353f75d6c51f2ab7ab63ad854b9373
furvietto <fulvio-94@hotmail.it>
2022-02-04 16:37:53 +0100

    add app

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <title>Document</title>
 </head>
-<body>
-    
+<body id="app">
+ciao
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="script.js"></script>
 </body>
 </html>
