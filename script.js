1aadf8e412a94e43729f6c624bc4758d9ea4e687
furvietto <fulvio-94@hotmail.it>
2022-02-04 16:23:19 +0100

    fix work

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const App = new Vue({
         cards : []
     },
     created() {
-        
+        axios.get('http://localhost:80/lezione-2-php/php-snacks-b1/controller.php').then((result) => {
+          this.cards = result.data.results;
+          console.log(result);
+        }).catch((error) => { console.log(error);});
     },
 })
